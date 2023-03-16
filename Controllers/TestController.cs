ad0164ebd9ac125ac3eea769bd0524c3e5b50105
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2023-03-16 08:49:35 +0100

    feat: Create GetCalculatedTaxes Get Method

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -59,5 +59,46 @@ namespace AS2223_4G_INF_IngrassiaSamuele_API.Controllers
 
             return Json(new { res = res, status = status, message = message });;
         }
+
+        [HttpGet("GetCalculatedTaxes")]
+        public JsonResult GetCalculatedTaxes(int salary)
+        {
+            float taxes;
+            string status = "OK";
+            string message = "";
+
+            if (salary <= 0)
+            {
+                taxes = 0;
+                status = "KO";
+                message = $"Errore: Il salario non può essere minore di 0";
+            }
+            else
+            {
+                try
+                {
+
+                    if (salary > 35000)
+                    {
+                        taxes = 35000 * 12 / 100; //Finds the 12% percentage number of 35000 as a fixed number (we are above 35000$ of salary)
+                        salary -= 35000;
+                        taxes += salary * 28 / 100;
+                    }
+                    else
+                    {
+                        taxes = salary * 12 / 100;
+                    }
+                }
+                catch (Exception e)
+                {
+                    taxes = 0;
+                    status = "KO";
+                    message = $"Errore: {e}";
+                }
+            }
+
+
+            return Json(new { res = taxes, status = status, message = message }); ;
+        }
     }
 }
