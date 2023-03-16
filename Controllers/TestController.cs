da77368901e3fc71124b44c443a803248341adb3
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2023-03-16 08:21:48 +0100

    feat: Create TestController.cs

---
+++ Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS2223_4G_INF_IngrassiaSamuele_API.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
