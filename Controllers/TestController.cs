9e1d5ce69e321b36627ec5f9761d61e56bf10904
Samuele Ingrassia <samuele.ingrassia@studenti.ittsrimini.edu.it>
2023-03-16 08:26:53 +0100

    feat: Create GetOddOrEven Get Method

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -2,11 +2,37 @@
 
 namespace AS2223_4G_INF_IngrassiaSamuele_API.Controllers
 {
+    [ApiController]
+    [Produces("application/json")]
+    [Route("api/v1/")]
+
     public class TestController : Controller
     {
-        public IActionResult Index()
+
+        [HttpGet("GetOddOrEven")]
+        public JsonResult GetOddOrEven(int number)
         {
-            return View();
+            string status = "OK";
+            string message;
+
+            try
+            {
+                if (number % 2 == 0)
+                {
+                    message = "Il numero è pari";
+                }
+                else
+                {
+                    message = "Il numero è dispari";
+                }
+            }
+            catch (Exception e)
+            {
+                status = "KO";
+                message = $"Errore: {e}";
+            }
+
+            return Json(new { status = status, message = message});
         }
     }
 }
